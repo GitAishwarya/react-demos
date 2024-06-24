9dbc6adfc7d3949e8ec4652f9a8701fc7282e25c
GitAishwarya <aishmagic942@gmail.com>
2024-06-25 00:25:43 +1000

    Adding changes to PDAS in ProfileCard.js

---
+++ src/ProfileCard.js
@@ -0,0 +1,12 @@
+function ProfileCard({ title, handle }) {
+  // concept - JS Destructuring
+
+  return (
+    <div>
+      <div> Title is {title}</div>
+      <div> Handle is {handle}</div>
+    </div>
+  );
+}
+
+export default ProfileCard;
