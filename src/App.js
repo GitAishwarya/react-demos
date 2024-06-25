3ee4354f7e939ad28f387a3bf9724498d0243ad4
GitAishwarya <aishmagic942@gmail.com>
2024-06-25 23:13:12 +1000

    Add PDA images to demo app1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import "bulma/css/bulma.css";
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <div>
-      <div>Profile Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Profile Digital Assistants</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-3">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was created by Amazon and helps you buy things."
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Cortana was created by Microsoft and helps"
+              />
+            </div>
+            <div className="column is-3">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was made by Apple and is being phased out"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
